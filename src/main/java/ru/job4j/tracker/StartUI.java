783b82de249821465aaf7ec3f880ad9ba7c9ecb5
Oklevet <nikstar1235@gmail.com>
2020-12-26 13:28:05 +0300

    9. toString [#430311]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,11 +4,7 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Tracker tracker = new Tracker();
-        Item item1 = new Item();
-        item1.setId(1);
-        item1.setName("test");
-        tracker.add(item1);
-        System.out.println(tracker.findById(1));
+        Item item = new Item();
+        System.out.println(item);
     }
 }
