63c778db67134da29b4e137e77ccb43145001746
Oklevet <nikstar1235@gmail.com>
2021-01-15 17:12:01 +0300

    2.0. Встроенные функциональные интерфейсы. [#430037]

--- src/main/java/ru/job4j/lambda/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -2,41 +2,19 @@ package ru.job4j.lambda;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.UnaryOperator;
+import java.util.function.Predicate;
 
 public class SearchAtt {
     private static List<Attachment> filter(List<Attachment> list,
-                                           UnaryOperator<List<Attachment>> func) {
+                                  Predicate<List<Attachment>> isPos) {
         List<Attachment> rsl = new ArrayList<>();
         for (Attachment att : list) {
-            rsl.add(func.apply(att));
+           if (att.getName().contains("bug")) {
+               rsl.add(att);
+           } else if (att.getSize() > 100) {
+               rsl.add(att);
+           }
         }
         return rsl;
-    }
-
-    public static List<Attachment> filterSize(List<Attachment> list) {
-        UnaryOperator<List<Attachment>> func = new UnaryOperator<List<Attachment>>() {
-            @Override
-            public List<Attachment> apply(List<Attachment> attachments) {
-                if (attachments.getSize() > 100) {
-                    return attachments;
-                }
-                return null;
-            }
-        };
-        return filter(list, func);
-    }
-
-    public static List<Attachment> filterName(List<Attachment> list) {
-        UnaryOperator<List<Attachment>> func = new UnaryOperator<List<Attachment>>() {
-            @Override
-            public List<Attachment> apply(List<Attachment> attachments) {
-                if (attachments.getName().contains("bug")) {
-                    return attachments;
-                }
-                return null;
-            }
-        };
-        return filter(list, func);
     }
 }
