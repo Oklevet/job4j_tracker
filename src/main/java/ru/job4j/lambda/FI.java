a3fc3cc8e12975c6f415651256e182507bb8925d
Oklevet <nikstar1235@gmail.com>
2021-01-15 16:19:14 +0300

    2.2. Лямбда блок [#430036]

--- src/main/java/ru/job4j/lambda/FI.java
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -1,20 +1,31 @@
 package ru.job4j.lambda;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 
 public class FI {
     public static void main(String[] args) {
         Attachment[] atts = {
-                new Attachment("image 1", 20),
-                new Attachment("image 3", 120),
+                new Attachment("image 1", 28),
+                new Attachment("image 3", 20),
                 new Attachment("image 2", 23)
         };
-        Comparator<String> cmpText = (left, right) ->  {
-            return left.compareTo(right);
+        Comparator<Attachment> cmpText = (left, right) ->  {
+            System.out.println("compare - from " + right.getSize()
+                    + "(maximum) to " + left.getSize() + "(minimum)");
+            return left.getSize() - (right.getSize());
         };
-        Comparator<String> cmpDescSize = (left, right) -> {
-            return right.length() - left.length();
+        Comparator<Attachment> cmpDescSize = (left, right) -> {
+            System.out.println("compare - from " + right.getName().length()
+                    + "(maximum) to " + left.getName().length() + "(minimum)");
+            return right.getName().length() - left.getName().length();
         };
+        Comparator<Attachment> comparator = (left, right)
+                -> left.getSize() - right.getSize();
+        Arrays.sort(atts, cmpText);
+        for (Attachment a : atts) {
+            System.out.println(a);
+        }
     }
 }
