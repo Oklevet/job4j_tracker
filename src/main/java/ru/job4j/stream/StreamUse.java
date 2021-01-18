b0998e8e9dc6b58bc73e6213795a72556b7f4f90
Oklevet <nikstar1235@gmail.com>
2021-01-18 19:54:25 +0300

    +

---
+++ src/main/java/ru/job4j/stream/StreamUse.java
@@ -0,0 +1,19 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUse {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        List<Integer> positives = new ArrayList<>();
+        nums.add(11);
+        nums.add(-21);
+        nums.add(51);
+        nums.add(-81);
+        nums.add(101);
+        positives = nums.stream().filter(x -> x > 0).collect(Collectors.toList());
+        positives.forEach(System.out::println);
+    }
+}
