f32258a4444da5afc27c15efdd4d4de392a78143
Oklevet <nikstar1235@gmail.com>
2021-01-22 19:28:42 +0300

    1. Метод .of [#430055]

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 public class NotifyAccountTest {
     @Test
     public void sent() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001")
         );
@@ -27,7 +27,7 @@ public class NotifyAccountTest {
 
     @Test
     public void sentInvalid() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("121", "qwe qwe", "eDer3432f"),
