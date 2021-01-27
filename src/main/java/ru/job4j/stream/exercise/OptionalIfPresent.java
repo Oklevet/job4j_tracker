91850395172aa0e3a73f125767f9439a1dbe078f
Oklevet <nikstar1235@gmail.com>
2021-01-27 14:28:07 +0300

    5.3. Optional. Метод ifPresent()

---
+++ src/main/java/ru/job4j/stream/exercise/OptionalIfPresent.java
@@ -0,0 +1,21 @@
+package ru.job4j.stream.exercise;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class OptionalIfPresent {
+    public static void ifPresent(int[] data) {
+        OptionalInt mx = max(data);
+        if(mx.isPresent()) {
+            System.out.println("Max: " + mx.getAsInt());
+        }
+//        max(data).stream()
+//                .findFirst()
+//                .isPresent(x -> System.out.println("Max: " + x.getAsInt()));
+    }
+
+    private static OptionalInt max(int[] data) {
+        OptionalInt m = Arrays.stream(data).reduce(Integer::max);
+        return m;
+    }
+}
