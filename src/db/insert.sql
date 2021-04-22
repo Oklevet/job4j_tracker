b5bd41498cbaacb5dfd1326a328e0fc1c41f6116
Oklevet <nikstar1235@gmail.com>
2021-04-22 17:22:43 +0300

    1. Sql схема для проекта Tracker. [#466856]

---
+++ src/db/insert.sql
@@ -0,0 +1,6 @@
+insert into items (id, name) values (1, 'item_1');
+insert into items (id, name) values (2, 'item_2');
+insert into items (id, name) values (3, 'item_3');
+insert into items (id, name) values (4, 'item_4');
+insert into items (id, name) values (5, 'item_5');
+ 
