41c7488964d1d681530bf7795d32930cfb4ae5d7
Oklevet <nikstar1235@gmail.com>
2023-10-11 14:33:24 +0300

    1. Lombok. Настройка IDEA [#504888]

---
+++ src/main/java/ru/job4j/lombock/LombokUsage.java
@@ -0,0 +1,9 @@
+package ru.job4j.lombock;
+
+public class LombokUsage {
+    public static void main(String[] args) {
+        var bird = new BirdData();
+        bird.setAge(1);
+        System.out.println(bird);
+    }
+}
--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,39 +1,10 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
 
+import lombok.Data;
+
+@Data
 public class Item {
     private int id;
     private String name;
-
-    public Item() {
-    }
-
-    public Item(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Item{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + '}';
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
