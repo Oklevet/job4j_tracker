19552c035c692651d882ad2264a5b0991107b662
Oklevet <nikstar1235@gmail.com>
2023-10-13 11:40:53 +0300

    3. HbmTracker [#287705 #452224]

--- src/main/java/ru/job4j/tracker2/HbmTracker.java
+++ src/main/java/ru/job4j/tracker2/HbmTracker.java
@@ -22,61 +22,91 @@ public class HbmTracker implements Store, AutoCloseable {
 
     @Override
     public Item add(Item item) {
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        try {
+            session.beginTransaction();
+            session.save(item);
+            session.getTransaction().commit();
+            session.close();
+            return item;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return null;
     }
 
     @Override
     public boolean replace(int id, Item item) {
-        session.beginTransaction();
-        session.update(item);
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            session.update(item);
+            session.getTransaction().commit();
+            session.close();
+            return true;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
         return false;
     }
 
     @Override
     public boolean delete(int id) {
-        session.beginTransaction();
-        Item item = new Item();
-        item.setId(id);
-        session.delete(item);
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            Item item = new Item();
+            item.setId(id);
+            session.delete(item);
+            session.getTransaction().commit();
+            session.close();
+            return true;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
         return false;
     }
 
     @Override
     public List<Item> findAll() {
-        session.beginTransaction();
-        List<Item> result =
-                session.createQuery("Item", Item.class).list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        try {
+            session.beginTransaction();
+            List<Item> result =
+                    session.createQuery("Item", Item.class).list();
+            session.getTransaction().commit();
+            session.close();
+            return result;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return null;
     }
 
     @Override
     public List<Item> findByName(String key) {
-        session.beginTransaction();
-        List<Item> result =
-                session.createQuery("from Item i where name = :key", Item.class)
-                        .setParameter("key", key).list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        try {
+            session.beginTransaction();
+            List<Item> result =
+                    session.createQuery("from Item i where name = :key", Item.class)
+                            .setParameter("key", key).list();
+            session.getTransaction().commit();
+            session.close();
+            return result;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return null;
     }
 
     @Override
     public Item findById(int id) {
-        session.beginTransaction();
-        Item result = session.get(Item.class, id);
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        try {
+            session.beginTransaction();
+            Item result = session.get(Item.class, id);
+            session.getTransaction().commit();
+            session.close();
+            return result;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return null;
     }
 
     @Override
