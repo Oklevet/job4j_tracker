309191726ecb92addbb29fdb2455d35de5063874
Oklevet <nikstar1235@gmail.com>
2023-10-16 10:03:31 +0300

    3. HbmTracker [#287705 #452224]

--- src/main/java/ru/job4j/tracker2/HbmTracker.java
+++ src/main/java/ru/job4j/tracker2/HbmTracker.java
@@ -13,100 +13,110 @@ public class HbmTracker implements Store, AutoCloseable {
             .configure().build();
     private final SessionFactory sf = new MetadataSources(registry)
             .buildMetadata().buildSessionFactory();
-    private Session session;
 
     @Override
     public void init() {
-        session = sf.openSession();
     }
 
     @Override
     public Item add(Item item) {
+        Session session = sf.openSession();
         try {
             session.beginTransaction();
             session.save(item);
             session.getTransaction().commit();
-            session.close();
-            return item;
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();         
         }
-        return null;
+        return item;
     }
 
     @Override
     public boolean replace(int id, Item item) {
+        Session session = sf.openSession();
         try {
             session.beginTransaction();
             session.update(item);
             session.getTransaction().commit();
-            session.close();
             return true;
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
         return false;
     }
 
     @Override
     public boolean delete(int id) {
+        Session session = sf.openSession();
+        boolean result = false;
         try {
             session.beginTransaction();
             Item item = new Item();
             item.setId(id);
             session.delete(item);
             session.getTransaction().commit();
-            session.close();
-            return true;
+            result = true;
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
-        return false;
+        return result;
     }
 
     @Override
     public List<Item> findAll() {
+        Session session = sf.openSession();
+        List<Item> result = null;
         try {
             session.beginTransaction();
-            List<Item> result =
-                    session.createQuery("Item", Item.class).list();
+             result = session.createQuery("Item", Item.class).list();
             session.getTransaction().commit();
-            session.close();
-            return result;
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
-        return null;
+        return result;
     }
 
     @Override
     public List<Item> findByName(String key) {
+        Session session = sf.openSession();
+        List<Item> result = null;
         try {
             session.beginTransaction();
-            List<Item> result =
-                    session.createQuery("from Item i where name = :key", Item.class)
+            result = session.createQuery("from Item i where name = :key", Item.class)
                             .setParameter("key", key).list();
             session.getTransaction().commit();
-            session.close();
-            return result;
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
         return null;
     }
 
     @Override
     public Item findById(int id) {
+        Session session = sf.openSession();
+        Item result = null;
         try {
             session.beginTransaction();
-            Item result = session.get(Item.class, id);
+            result = session.get(Item.class, id);
             session.getTransaction().commit();
             session.close();
             return result;
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
-        return null;
+        return result;
     }
 
     @Override
